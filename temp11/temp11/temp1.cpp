c3c99a4df7a1e7a3ed772ef71116cef991856514
harris_91 <harrislee1020@gmail.com>
2023-10-09 17:49:17 +0900

    add saimple ops

--- temp11/temp11/temp1.cpp
+++ temp11/temp11/temp1.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
-
-printf("hello Mingu Mr GARAM");
-printf("hello harris Mr lee");
-return 0;
+	int x = 3;
+	int y = 5;
+	int z = 9;
+	printf("%d", x + y + z);
+	printf("hello Mingu Mr GARAM");
+	printf("hello harris Mr lee");
+	return 0;
 
 }
